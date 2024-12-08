e8a303723f246f885a055aa955e811460dfdf367
zap24 <zap24@inbox.ru>
2024-12-08 18:35:45 +0300

    Create new project

---
+++ src/main/java/com/example/ReleaseStepsBot/ReleaseStepsBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReleaseStepsBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseStepsBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseStepsBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReleaseStepsBot/ReleaseStepsBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReleaseStepsBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseStepsBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
